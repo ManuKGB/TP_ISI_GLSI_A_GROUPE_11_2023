bb9d4009f586d0e9ab89775051c1ddcdf48a77a2
ManuKGB <kgb.corporation.2022@gmail.com>
2023-05-04 20:12:05 +0000

    initialisation et configuration du projet initial

---
+++ src/main/java/com/ega/ega_api/EgaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ega.ega_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ega/ega_api/EgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ega.ega_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
